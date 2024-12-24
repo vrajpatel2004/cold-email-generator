7e1b4c239376263cdf14257b9a7e40e23d2a59fc
vrajpatel2004 <114735279+vrajpatel2004@users.noreply.github.com>
2024-12-24 19:07:46 +0530

    Update emai_generator.py

--- emai_generator.py
+++ emai_generator.py
@@ -11,29 +11,18 @@ def create_model(groq_api):
     llm = ChatGroq(
     temperature = 0.0,
     groq_api_key = groq_api,
-    model="llama-3.3-70b-versatile",
+    model = "llama-3.3-70b-versatile",
     )      
     return llm
 
-def fetch_data_from_url(url_link):
-    loader = WebBaseLoader(url_link)
+def fetch_data_from_url(job_url):
+    loader = WebBaseLoader(job_url)
     page_data = loader.load().pop().page_content
     #print(page_data)
     return page_data
 
-def access_perticular_data(llm, page_data):
-    prompt_extract = PromptTemplate.from_template(
-        """
-        ### SCRAPED TEXT FROM WEBSITE:
-        {page_data}
-        ### INSTRUCTION:
-        the screaped text is from the career page of a website.
-        your job is to extract the job postings and return them in JSON format containing
-        following keys: 'role', 'experience', 'skills' and 'description'.
-        only return the valid JSON.
-        ### VALID JSON (NO PRAMBLE):
-        """
-    )
+def access_perticular_data(llm, page_data, data_extract):
+    prompt_extract = PromptTemplate.from_template(data_extract)
     chain_extract = prompt_extract | llm  # give data to the llm  
     #print(chain_extract)
     res = chain_extract.invoke(input={'page_data':page_data})
@@ -62,33 +51,15 @@ def find_job_profile(job, collection) :
     query_texts=job['skills'], n_results=2).get('metadatas', [])
     return links
 
-def create_email(llm, job, links):
-    prompt_email = PromptTemplate.from_template(
-        """
-        ### JOB DESCRIPTION:
-        {job_description}
-        
-        ### INSTRUCTION:
-        You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
-        the seamless integration of business processes through automated tools. 
-        Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
-        process optimization, cost reduction, and heightened overall efficiency. 
-        Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
-        in fulfilling their needs.
-        Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-        Remember you are Mohan, BDE at AtliQ. 
-        Do not provide a preamble.
-        ### EMAIL (NO PREAMBLE):
-        
-        """
-        )
+def create_email(llm, job, links, output_email):
+    prompt_email = PromptTemplate.from_template(output_email)
 
     chain_email = prompt_email | llm
     res = chain_email.invoke({"job_description": str(job), "link_list": links})
     return res.content
 
 
-def main(groq_api, my_portfolio, job_url):
+def main(groq_api, my_portfolio, job_url, data_extract, output_email):
     # create llm model
     llm_model = create_model(groq_api)
     
@@ -96,8 +67,8 @@ def main(groq_api, my_portfolio, job_url):
     url_all_content= fetch_data_from_url(job_url)
     #print(url_all_content)
     
-    #extract job posting data from all content
-    data = access_perticular_data(llm_model, url_all_content)
+    #extract job posting data from html content
+    data = access_perticular_data(llm_model, url_all_content, data_extract)
     #print(data)
     
     #convert data into json
@@ -119,15 +90,44 @@ def main(groq_api, my_portfolio, job_url):
     links = find_job_profile(job, collection)
     #print(links)
     
-    email = create_email(llm_model, job, links)
+    email = create_email(llm_model, job, links, output_email)
     print(email)
 
 
 if __name__ == "__main__":
     
     
-    groq_api     = "your_api_key"      
+    groq_api     = "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"      
     my_portfolio = "./my_portfolio.csv"
     job_url      = "https://jobs.nike.com/job/R-43293?from=job%20search%20funnel"
-           
-    main(groq_api, my_portfolio, job_url)
+    
+    data_extract =   """
+                     ### SCRAPED TEXT FROM WEBSITE:
+                     {page_data}
+                     ### INSTRUCTION:
+                     the screaped text is from the career page of a website.
+                     your job is to extract the job postings and return them in JSON format containing
+                     following keys: 'role', 'experience', 'skills' and 'description'.
+                     only return the valid JSON.
+                     ### VALID JSON (NO PRAMBLE):
+                     
+                     """               
+    output_email =   """                    
+                     ### JOB DESCRIPTION:
+                     {job_description}
+                     
+                     ### INSTRUCTION:
+                     You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
+                     the seamless integration of business processes through automated tools. 
+                     Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
+                     process optimization, cost reduction, and heightened overall efficiency. 
+                     Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
+                     in fulfilling their needs.
+                     Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
+                     Remember you are Mohan, BDE at AtliQ. 
+                     Do not provide a preamble.
+                     ### EMAIL (NO PREAMBLE):
+                                    
+                     """
+            
+    main(groq_api, my_portfolio, job_url, data_extract, output_email)
